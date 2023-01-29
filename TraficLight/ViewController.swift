2d93fb49e8db9e7ef646f58dfcb26588c126006c
Liza Grossman <lisok.sam@gmail.com>
2023-01-29 21:40:23 +0200

    Initial Commit

---
+++ TraficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraficLight
+//
+//  Created by Elizabeth on 29/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
