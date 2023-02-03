c34fe747af5652ce006572bdbce84495ea105cbf
Liza Grossman <lisok.sam@gmail.com>
2023-02-03 15:05:27 +0200

    Final version: No Problem with corners Made with switch Min constraints

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -7,40 +7,65 @@
 
 import UIKit
 
+enum CurrentTrafficLight{
+    case red
+    case yellow
+    case green
+}
+
+
 class ViewController: UIViewController {
-    @IBOutlet var greenSign: UIView!
-    @IBOutlet var yellowSign: UIView!
-    @IBOutlet var redSign: UIView!
-    @IBOutlet var nextColorButton: UIButton!
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
     
-     
+    private var currentLight = CurrentTrafficLight.red
+    private var lightIsOn: CGFloat = 1
+    private var lightIsOf: CGFloat = 0.3
+    
+    @IBOutlet var buttonNext: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        greenSign.layer.cornerRadius =  greenSign.frame.size.height / 2
-        yellowSign.layer.cornerRadius = yellowSign.frame.size.width / 2
-        redSign.layer.cornerRadius = redSign.frame.size.width / 2
+        buttonNext.layer.cornerRadius = 10
         
-        greenSign.alpha = 0.5
-        yellowSign.alpha = 0.5
-        redSign.alpha = 0.5
+        redLight.alpha = lightIsOf
+        yellowLight.alpha = lightIsOf
+        greenLight.alpha = lightIsOf
     }
     
-    @IBAction func changeColor() {
-        nextColorButton.setTitle("NEXT", for:.normal)
-        if redSign.alpha == 1 {
-            redSign.alpha = 0.5
-            yellowSign.alpha = 1
-        } else if yellowSign.alpha == 1 {
-            yellowSign.alpha = 0.5
-            greenSign.alpha = 1
-        } else if greenSign.alpha == 1 {
-            greenSign.alpha = 0.5
-            redSign.alpha = 1
-        } else {
-            redSign.alpha = 1
-        }
-        }
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
     }
+    
+    @IBAction func changeLight() {
+        if buttonNext.currentTitle == "START" {
+            buttonNext.setTitle("NEXT", for: .normal)
+        }
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOf
+            redLight.alpha = lightIsOn
+            yellowLight.alpha = lightIsOf
+            currentLight = .yellow
+        case .yellow:
+            greenLight.alpha = lightIsOf
+            redLight.alpha = lightIsOf
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightIsOn
+            redLight.alpha = lightIsOf
+            yellowLight.alpha = lightIsOf
+            currentLight = .red
+        }
+            
+    }
+        
+}
+    
     
 
 
